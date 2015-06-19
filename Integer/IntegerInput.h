135cf001a208cbd7dca6ebc85339045f81738451
T-Santos <1tsantos7@gmail.com>
2015-06-18 21:09:05 -0400

    Input Utilities
    Various input utilities to validate input prior to execution.

---
+++ Integer/IntegerInput.h
@@ -0,0 +1,12 @@
+#ifndef INTEGERINPUT_H_INCLUDED
+#define INTEGERINPUT_H_INCLUDED
+
+// Include depends
+#include <string>
+#include <iostream>
+#include <sstream>
+
+//TODO overload function w/o message?
+int getInt();
+
+#endif // INTEGERINPUT_H_INCLUDED
